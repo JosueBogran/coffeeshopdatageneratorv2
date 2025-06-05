2a99993b6bca94c0bc04fae7c695e86cd152add1
JosueBogran <46737143+JosueBogran@users.noreply.github.com>
2025-06-05 18:03:43 -0400

    Update Spreader.sql

--- Spreader.sql
+++ Spreader.sql
@@ -3,10 +3,13 @@
 CREATE SCHEMA IF NOT EXISTS sweetcoffeetree.coffeesales500m_v2_small;
 
 CREATE TABLE sweetcoffeetree.coffeesales500m_v2_small.fact_sales
+CLUSTER BY (order_date, product_name, location_id)
 AS
 SELECT *
 FROM
-sweetcoffeetree.coffeesales500m_v2_seed.fact_sales;
+sweetcoffeetree.coffeesales500m_v2_seed.fact_sales
+ORDER BY order_date ASC, product_name ASC, location_id ASC;
+;
 
 CREATE TABLE sweetcoffeetree.coffeesales500m_v2_small.dim_products
 AS
